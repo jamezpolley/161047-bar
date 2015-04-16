cb17effe12b60a4e3f28f932d5b07bd7db9df14a
James Polley <jp@jamezpolley.com>
2015-04-16 12:14:01 +1000

    initial creation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='bar',
+      version='2.0',
+      description='bar',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['bar'],
+      zip_safe=False,
+  )
